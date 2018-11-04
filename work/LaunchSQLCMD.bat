2a0d6c868fa2109fd98dc2d78a44c7abb2ec7b88
0xShellcode <BDAthans@yahoo.com>
2018-11-04 02:30:39 -0600

    Update LaunchSQLCMD.bat

--- work/LaunchSQLCMD.bat
+++ work/LaunchSQLCMD.bat
@@ -2,7 +2,7 @@
 color 0F
 mode con lines=80 cols=170
 cd C:\Program Files\Microsoft SQL Server\100\Tools\Binn\
-SET SQLCMDPASSWORD=OMateSQL@2007
+SET SQLCMDPASSWORD=GOES_HERE
 echo Microsoft (R) SQL Server Command Line Tool
 echo Version 10.50.1600.1 NT INTEL X64
 echo.
